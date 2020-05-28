854a9c4bf419a0c6d8b1d55313e0f047a568848e
vspv <seva--seva@ukr.net>
2020-05-28 16:19:46 +0300

    Updates

---
+++ precache-manifest.4622aa53cc974a474f581557d1f988af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba35c1716a8909beea332a506c9cd16",
+    "url": "/crypto-converter/index.html"
+  },
+  {
+    "revision": "c1455016eb198ea38b37",
+    "url": "/crypto-converter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "794e20e71f5382dba4be",
+    "url": "/crypto-converter/static/js/2.8630db0a.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/crypto-converter/static/js/2.8630db0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1455016eb198ea38b37",
+    "url": "/crypto-converter/static/js/main.6d0f9b7a.chunk.js"
+  },
+  {
+    "revision": "46c7f93001d22dc139ee",
+    "url": "/crypto-converter/static/js/runtime-main.415bafdd.js"
+  }
+]);
